10d26551720c6fbe1a67416ef8fb61cf11585513
ZJAC178 <rohitkakkar@Rohits-MBP.broadband>
2024-03-07 18:36:49 +0000

    Heuristic function updated to support cell input with testing

--- PDDL parser/pathfinding.py
+++ PDDL parser/pathfinding.py
@@ -1,6 +1,6 @@
 #Manhattan distance used for heuristic
 def h(cell1,cell2):
-    x1,y1=cell1
-    x2,y2=cell2
+    x1, y1 = int(cell1[4]), int(cell1[5])
+    x2, y2 = int(cell2[4]), int(cell2[5])
 
     return abs(x1-x2) + abs(y1-y2)
--- PDDL parser/pathfinding_test.py
+++ PDDL parser/pathfinding_test.py
@@ -2,12 +2,23 @@ import unittest
 from pathfinding import h
 
 class TestHeuristic(unittest.TestCase):
-    def test_manhattendistance(self):
-        self.assertEqual(h((0, 0), (0, 5)), 5)
-        
-    def test_incorrectinput(self):
-        with self.assertRaises(TypeError):
-            h(("a", "b"), (0, 1))
+    def test_adjacent_horizontal(self):
+        self.assertEqual(h("cell00", "cell01"), 1)
+
+    def test_adjacent_vertical(self):
+        self.assertEqual(h("cell00", "cell10"), 1)
+
+    def test_diagonally_adjacent(self):
+        self.assertEqual(h("cell00", "cell11"), 2)
+
+    def test_non_adjacent(self):
+        self.assertEqual(h("cell00", "cell33"), 6)
+
+    def test_same_cell(self):
+        self.assertEqual(h("cell22", "cell22"), 0)
+
+    def test_non_zero_cells(self):
+        self.assertEqual(h("cell23", "cell45"), 4)
     
 if __name__ == '__main__':
     unittest.main()
