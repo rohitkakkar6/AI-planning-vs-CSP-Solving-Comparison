b29caf72277bc47e370e46b069bbcf9a5d7a4200
zjac178 <ZJAC178@live.rhul.ac.uk>
2024-04-11 13:59:21 +0100

    completed PDB generation through breadth first search from goal

--- PDDL_parser/PDB.py
+++ PDDL_parser/PDB.py
@@ -1,3 +1,4 @@
+from collections import deque
 from pddl_parser import *
 from aStar import *
 import re
@@ -78,6 +79,247 @@ def filter_adjacencies_for_chunk(chunk, right_of, left_of, is_above, is_undernea
             filtered["is_underneath"][cell] = is_underneath[cell]
     return filtered
 
+def localise_cell(cell, offset_x, offset_y):
+        parts = cell.split('x')[1].split('y')
+        cell_x, cell_y = int(parts[0]), int(parts[1])
+        
+        local_x = cell_x - offset_x
+        local_y = cell_y - offset_y
+        
+        return f"cellx{local_x}y{local_y}"
+
+def localise(chunk, entry, exit, right_adj, left_adj, above_adj, under_adj):
+    
+    x, y = chunk
+    offset_x = x * 3
+    offset_y = y * 3
+
+    entry_x, entry_y = entry
+    exit_x, exit_y = exit
+    
+    entry_x = entry_x - offset_x
+    entry_y = entry_y - offset_y
+    local_entry = (entry_x, entry_y)
+
+    exit_x = exit_x - offset_x
+    exit_y = exit_y - offset_y
+    local_exit = (exit_x, exit_y)
+
+    localized_right_adjacency = {localise_cell(key, offset_x, offset_y): localise_cell(value, offset_x, offset_y) for key, value in right_adj.items()}
+    localized_left_adjacency = {localise_cell(key, offset_x, offset_y): localise_cell(value, offset_x, offset_y) for key, value in left_adj.items()}
+    localized_above_adjacency = {localise_cell(key, offset_x, offset_y): localise_cell(value, offset_x, offset_y) for key, value in above_adj.items()}
+    localized_under_adjacency = {localise_cell(key, offset_x, offset_y): localise_cell(value, offset_x, offset_y) for key, value in under_adj.items()}
+    
+    return localized_right_adjacency, localized_left_adjacency, localized_above_adjacency, localized_under_adjacency, local_exit, local_entry
+
+def process_adjacency(direction, cells):
+        sorted_cells = sorted(cells.items(), key=lambda item: item[0])
+        return [f"{direction}-{key}-{value}" for key, value in sorted_cells]
+
+def generate_lookup_key(right_of, left_of, is_above, is_underneath, entry_points, exit_points):
+    adjacencies_str_parts = []
+    adjacencies_str_parts.extend(process_adjacency("right_of", right_of))
+    adjacencies_str_parts.extend(process_adjacency("left_of", left_of))
+    adjacencies_str_parts.extend(process_adjacency("above", is_above))
+    adjacencies_str_parts.extend(process_adjacency("underneath", is_underneath))
+
+    adjacencies_str = ",".join(adjacencies_str_parts)
+
+    entry_points_str = ",".join(sorted([f"entry-{pt[0]}-{pt[1]}" for pt in entry_points]))
+    exit_points_str = ",".join(sorted([f"exit-{pt[0]}-{pt[1]}" for pt in exit_points]))
+
+    lookup_key = f"{adjacencies_str}|{entry_points_str}|{exit_points_str}"
+    return lookup_key
+
+def globalise_path(chunk_coord, path):
+    globalised_path = []
+    x, y = chunk_coord
+    offset_x = x * 3
+    offset_y = y * 3
+    for cell in path:
+        parts = cell.split('x')[1].split('y')
+        cell_x, cell_y = int(parts[0]), int(parts[1])
+        
+        global_x = cell_x + offset_x
+        global_y = cell_y + offset_y
+        
+        globalised_cell = f"cellx{global_x}y{global_y}"
+        globalised_path.append(globalised_cell)
+    
+    return globalised_path
+
+def localise_path(chunk_coord, path):
+    localised_path = []
+    x, y = chunk_coord
+    offset_x = x * 3
+    offset_y = y * 3
+    for cell in path:
+        parts = cell.split('x')[1].split('y')
+        cell_x, cell_y = int(parts[0]), int(parts[1])
+        
+        global_x = cell_x - offset_x
+        global_y = cell_y - offset_y
+        
+        localised_cell = f"cellx{global_x}y{global_y}"
+        localised_path.append(localised_cell)
+    
+    return localised_path
+
+
+def calculate_paths_for_chunk(chunk, chunk_coord, right_of, left_of, is_above, is_underneath):
+    filtered_adjacencies = filter_adjacencies_for_chunk(chunk, right_of, left_of, is_above, is_underneath)
+    entry_points = chunk.get_entry_points()
+    exit_points = chunk.get_exit_points()
+    path_db = {}
+
+    new_x, new_y = chunk_coord
+    top_left_x_new = new_x * 3
+    top_left_y_new = new_y * 3
+    for entry in entry_points:
+        for exit in exit_points:
+            lright_adj, lleft_adj, labove_adj, lunder_adj, lexit, lentry = localise(chunk_coord, entry, exit, filtered_adjacencies["right_of"], filtered_adjacencies["left_of"], filtered_adjacencies["is_above"], filtered_adjacencies["is_underneath"])
+            lookup_key = generate_lookup_key(lright_adj, lleft_adj, labove_adj, lunder_adj, [lentry], [lexit])
+            existing_path = path_db.get(lookup_key, None)
+            if existing_path is not None:
+                new_path = globalise_path(chunk_coord, existing_path)
+                cost = len(new_path)
+                chunk.add_path(entry, exit, new_path, cost)
+            else:
+                start_cell = f"cellx{entry[0]}y{entry[1]}"
+                goal_cell = f"cellx{exit[0]}y{exit[1]}"
+                path = aStar(filtered_adjacencies["right_of"], filtered_adjacencies["left_of"], filtered_adjacencies["is_above"], filtered_adjacencies["is_underneath"], start_cell, goal_cell, 3, (top_left_x_new, top_left_y_new))
+                if path is not None:
+                    new_path = localise_path(chunk_coord, path)
+                    path_db[lookup_key] = new_path
+                    cost = len(new_path)
+                    chunk.add_path(entry, exit, path, cost)
+
+def create_chunk_graph(chunk_map):
+    graph = {}
+    for chunk_coord, chunk in chunk_map.items():
+        graph[chunk_coord] = {}
+        for entry, exits in chunk.paths.items():
+            graph[chunk_coord][entry] = {}
+            for exit, details in exits.items():
+                path = details['path']
+                cost = details['cost']
+                direction = chunk.entry_points.get(entry)
+                graph[chunk_coord][entry][exit] = {'path': path, 'cost': cost, 'direction': direction}
+    return graph
+
+def adjacent_cell_and_chunk(path, direction, chunk):
+    cell = path[0]
+    match = re.match(r'cellx(\d+)y(\d+)', cell)
+    
+    cell_x, cell_y = map(int, match.groups())
+    chunk_x, chunk_y = chunk
+    
+    if direction == 'up':
+        return f"cellx{cell_x}y{cell_y - 1}", (chunk_x, chunk_y - 1)
+    elif direction == 'down':
+        return f"cellx{cell_x}y{cell_y + 1}", (chunk_x, chunk_y + 1)
+    elif direction == 'left':
+        return f"cellx{cell_x - 1}y{cell_y}", (chunk_x - 1, chunk_y)
+    elif direction == 'right':
+        return f"cellx{cell_x + 1}y{cell_y}", (chunk_x + 1, chunk_y)
+    else:
+        return None, None
+
+
+def adjacent_path(adj_cell, adj_chunk, graph):
+    match = re.match(r'cellx(\d+)y(\d+)', adj_cell)
+    paths = []
+    x, y = map(int, match.groups())
+    cell = (x, y)
+    # Retrieve the adjacent chunk's data from the graph; if not found, default to an empty dict
+    chunk_data = graph.get(adj_chunk, {})
+    
+    for entry, exits in chunk_data.items():
+        for exit, details in exits.items():
+            if exit == cell:
+                adj_info = adjacent_cell_and_chunk(details['path'], details['direction'], adj_chunk)
+                paths.append((details['path'], adj_info, details['direction']))
+    
+    # Return the list of paths and costs
+    return paths
+
+
+def backtrack(graph):
+    max_chunk_coord = max(graph.keys(), key=lambda x: (x[0], x[1]))
+    start = max_chunk_coord
+    start_paths = []
+    queue = deque(start_paths)
+    all_paths = []
+
+    for entry, exits in graph[start].items():
+        for exit, details in exits.items():
+            current_path_info = []
+            direction = details['direction']
+            path = details['path']
+            reversed_path = path[::-1]
+            direction = details['direction']
+            start_paths.append((start, reversed_path, direction))
+            queue = deque(start_paths)
+
+    while queue:
+        chunk, path, direction = queue.popleft()  # Get the current path to extend
+        current_step = path[-1]  # Current step is the last element of the path
+        current_chunk = chunk  # Extract chunk from the current step
+
+        # Check if the current step is the goal
+        if current_chunk == (0, 0):
+            final_cell, final_chunk = adjacent_cell_and_chunk(path[::-1], direction, chunk)
+
+            if final_cell:
+                adjacent = adjacent_path(final_cell, current_chunk, graph)
+                if adjacent:
+                    for adj_path, (new_adj_cell, new_adj_chunk), adj_direction in adjacent:
+                        path = path + adj_path[::-1]
+
+            all_paths.append(path)  # Add the complete path to all_paths
+            continue
+        
+        adj_cell, chunknew = adjacent_cell_and_chunk(path[::-1], direction, chunk)
+        if chunk == (start):
+            chunk = chunknew
+        adj_chunk = chunk
+        if adj_cell is None or adj_chunk is None:
+            continue  # No valid adjacency found
+
+        adjacent = adjacent_path(adj_cell, adj_chunk, graph)
+
+        for adj_path, adj_info, adj_direction in adjacent:
+            adj_paths = [adj_path for adj_path, _, _ in adjacent]
+            adj_infos = [adj_info for _, adj_info, _ in adjacent]
+            adj_direction = [adj_direction for _, _, adj_direction in adjacent]
+            new_adj_cell, current_chunk = adj_infos[0]
+            for adj_path, (new_adj_cell, new_adj_chunk), adj_direction in adjacent:
+                new_path = path + adj_path[::-1]
+                queue.append((new_adj_chunk, new_path, adj_direction))
+
+    return all_paths
+
+def target_path(paths, target):
+    target_paths = [path for path in paths if target in path]
+    shortest_path_result = None
+    if target_paths:
+        shortest_length = float('inf')
+        
+        for path in target_paths:
+            if target in path:
+                target_index = path.index(target)
+                path_to_target = path[:target_index + 1]
+                
+                if len(path_to_target) < shortest_length:
+                    shortest_length = len(path_to_target)
+                    shortest_path_result = path_to_target[::-1]
+                
+        cost = len(shortest_path_result)
+        return shortest_path_result, cost
+    else:
+        return None
+
+
 
 right_of, left_of, is_above, is_underneath, start, goal = parse_pddl_file('PDDL pathfinding/problem.pddl')
 all_cells = set(right_of.keys()) | set(left_of.keys()) | set(is_above.keys()) | set(is_underneath.keys())
@@ -91,3 +333,22 @@ for cell in all_cells:
     chunk_coord = cell_to_chunk(x, y)
     chunk_map[chunk_coord].add_cell(cell)
 identify_and_store_entry_exit_points(chunk_map, right_of, left_of, is_above, is_underneath)
+first_chunk_coord = (0, 0)
+if first_chunk_coord in chunk_map:
+    chunk_map[first_chunk_coord].add_entry_point((0, 0), 'manual')
+# Finding the coordinates of the last (bottom-right) chunk
+max_chunk_coord = max(chunk_map.keys())
+max_chunk_x, max_chunk_y = max_chunk_coord
+max_inside_chunk_x = (max_chunk_x * 3) + (3 - 1)
+max_inside_chunk_y = (max_chunk_y * 3) + (3 - 1)
+chunk_map[max_chunk_coord].add_exit_point((max_inside_chunk_x, max_inside_chunk_y), 'manual')
+# Calculate paths for each chunk
+for chunk_coord, chunk in chunk_map.items():
+    calculate_paths_for_chunk(chunk, chunk_coord, right_of, left_of, is_above, is_underneath)
+graph = create_chunk_graph(chunk_map)
+
+
+all_paths = backtrack(graph)
+target = "cellx0y0"
+end_path = target_path(all_paths, target)
+print("Shortest path and its cost:", end_path)
